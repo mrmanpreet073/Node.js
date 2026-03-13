9f90aa56fbdc6a4885a30f370665083b7bdbf9f1
Manpreet Singh <mrmanpreet073@gmail.com>
2026-03-13 14:19:39 +0530

    Typescript Practice

--- Todo/src/Validation/todo.schema.ts
+++ Todo/src/Validation/todo.schema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod"
 
-// Zod schema -> Runtime validation
+// Zod schema -> Runtime validation(checking incoming data when the app runs)
 export const todoValidationSchema = z.object({
   id: z.string().describe("Id of the Todo"),
   title: z.string().describe("Title of the todo"),
@@ -8,5 +8,5 @@ export const todoValidationSchema = z.object({
   isComplete: z.boolean().default(false).describe("Todo is completed or not")
 })
 
-// TypeScript type inferred from the schema (compile-time)
+// TypeScript type inferred from the schema (compile-time)(TypeScript type safety while coding)
 export type Todo = z.infer<typeof todoValidationSchema>
