84fb1268569c79f6d74fc74f2c985076549181f6
Manpreet Singh <mrmanpreet073@gmail.com>
2026-03-17 00:05:11 +0530

    Express

--- Express/express_intro.js
+++ Express/express_intro.js
@@ -24,9 +24,9 @@ function block_1_basicServer() {
         })
 
         app.get('/search', (req, res) => {
-            const { q, limit } = req.body
+            const { query, limit } = req.query
             res.json({
-                query: q,
+                query,
                 limit: limit || '10'
             })
         })
@@ -37,7 +37,7 @@ function block_1_basicServer() {
                 price: 149
             })
         })
-        app.get('/order', (req, res) => {
+        app.post('/order', (req, res) => {
             const order = req.body
             res.status(201).json({
                 ststus: 'created',
@@ -57,14 +57,14 @@ function block_1_basicServer() {
                 //.json() is a method of the Response object returned by the Fetch API.
                 //read the response body and convert JSON data into a JavaScript object.
 
-                console.log('Menu Result : ', JSON.stringify(menudata));
+                console.log('GET / Menu Result : ', JSON.stringify(menudata));
 
                 console.log("++++++++++++++++++++++++++++++++++++++++++")
 
 
-                const searchres = await fetch(`${base}/search?q=biryani&limit=5`);
+                const searchres = await fetch(`${base}/search?query=biryani&limit=5`);
                 const searchData = await searchres.json()
-             //why use await here ----- ---------------- 
+                //why use await here ----- ---------------- 
                 //                       searchres.json()  returns a Promise, not the actual data.
                 //                                            the browser must:
                 //                                            Read the body stream
@@ -74,34 +74,59 @@ function block_1_basicServer() {
                 //                                            Because reading the stream takes time, it is asynchronous.
                 //                                            So .json() returns a Promise. that's why using await 
                 //     
-                console.log('Menu Result : ', JSON.stringify(searchData));
+                console.log('GET / Search Result : ', JSON.stringify(searchData));
 
                 console.log("++++++++++++++++++++++++++++++++++++++++++")
 
-                
+                const menuItemRes = await fetch(`${base}/menu/42`);
+                const menuItemData = await menuItemRes.json();
+
+                console.log('Get / Order Result : ', JSON.stringify(menuItemData));
+                console.log("++++++++++++++++++++++++++++++++++++++++++")
+
+                const orderres = await fetch(`${base}/order`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        item: 'biryani',
+                        qty: 2
+                    })
+                });
+                const orderData = await orderres.json()
+
+                console.log('POST / Order Result : ', JSON.stringify(orderData));
+
 
 
             } catch (error) {
 
+                console.log('error:', error);
 
             }
+            server.close(() => {
+                console.log('Block 1 served ');
+                resolev()
+            })
         })
 
 
 
     })
-
-    async function main() {
-
-        await block_1_basicServer()
-
-
-        process.exit(0)
-        // ---------------  0	Successful execution
-        //        |          1    Error / failure
-        //        |
-        //immediately terminates the running Node.js process.
-        // It stops the event loop, cancels pending operations, and exits the program.
-
-    }
 }
+
+async function main() {
+
+    await block_1_basicServer()
+
+
+    process.exit(0)
+    // ---------------  0	Successful execution
+    //        |          1    Error / failure
+    //        |
+    //immediately terminates the running Node.js process.
+    // It stops the event loop, cancels pending operations, and exits the program.
+
+}
+main()
