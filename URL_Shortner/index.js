124acbfba0c4080b8b6d40790343d8e78877ebee
Manpreet Singh <mrmanpreet073@gmail.com>
2026-03-27 21:54:13 +0530

    Url Shortner

--- URL_Shortner/index.js
+++ URL_Shortner/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import router from './routes/url.js';
 import connection from './connection.js';
+import URL from './models/url.js';
 
 
 const app = express();
@@ -9,11 +10,23 @@ const PORT = 8000;
 
 
 connection('mongodb://127.0.0.1:27017/short-url')
-.then(() => console.log('mongodb connected'))
-.catch((e) => console.log('Error', e))
+    .then(() => console.log('mongodb connected'))
+    .catch((e) => console.log('Error', e))
 
 
 app.use('/url', router)
+
+app.get('/:shortId', async (req, res) => {
+
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate({ shortId }, {
+        $push: {
+            visitHistory: { timestamps: Date.now() }
+        }
+    })
+
+    res.redirect(entry.redirectUrl)
+})
 
 app.listen(PORT, () => (
     console.log(`Server Listen at Port number ${PORT}`)
