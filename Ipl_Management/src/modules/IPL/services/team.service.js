1f8771f6e610d78bc0b7da69715efb6c5627d792
Manpreet Singh <mrmanpreet073@gmail.com>
2026-05-12 20:45:30 +0530

    team update

--- Ipl_Management/src/modules/IPL/services/team.service.js
+++ Ipl_Management/src/modules/IPL/services/team.service.js
@@ -1,23 +1,14 @@
 import apiError from "../../../common/utils/apiError.js";
 import Team from "../models/team.model.js";
 
-
-
-
-
 const createTeam = async ({ name, ownerId }) => {
 
     try {
-        const team = await Team.create({ name,ownerId});
-        if (!team) {
-            throw apiError.badRequest("team Not Created")
-        }
+        const team = await Team.create({ name, ownerId });
         return team
-
     } catch (error) {
         throw error
     }
-
 }
 
-export {createTeam}
+export { createTeam }
