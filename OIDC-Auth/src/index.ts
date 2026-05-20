5ef1ce7bb97401f81125d7eb643af2c53c2eb2f9
Manpreet Singh <mrmanpreet073@gmail.com>
2026-05-20 17:04:26 +0530

    key generation

--- OIDC-Auth/src/index.ts
+++ OIDC-Auth/src/index.ts
@@ -9,6 +9,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public")); //"Serve files from the public folder"
 
 
 app.get("/", (req: Request, res: Response) => {
@@ -29,9 +31,18 @@ app.get("/.well-known/openid-configuration", (req: Request, res: Response) => {
   });
 });
 
+app.get("/home", (req: Request, res: Response) => {
+  res.sendFile("home.html", { root: "./public" });
+});
 app.get("/signup", (req: Request, res: Response) => {
   res.sendFile("signup.html", { root: "./public" });
 });
+app.get("/login", (req: Request, res: Response) => {
+  res.sendFile("login.html", { root: "./public" });
+});
+
+
+
 
 app.post("/signup", async (req: Request, res: Response) => {
 
@@ -41,7 +52,6 @@ app.post("/signup", async (req: Request, res: Response) => {
     res.status(400).json({ message: "All fields are required." });
     return;
   }
-
   const [existing] = await db
     .select()
     .from(usersTable)
@@ -51,21 +61,48 @@ app.post("/signup", async (req: Request, res: Response) => {
   if (existing) {
     return res.send("User already exists");
   }
-
-  const  hashPassword = await bcrypt.hash(password,10)  // In production, hash the password before storing
+  const hashPassword = await bcrypt.hash(password, 10)  // In production, hash the password before storing
   await db.insert(usersTable).values({
     firstName,
     lastName,
     email,
     password: hashPassword, // In production, hash the password before storing
   });
-
   res.send("User Registered");
-
 });
 
 
+app.post("/o/login", async (req: Request, res: Response) => {
 
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.send("Email or Password are required")
+  }
+
+  const [user] = await db.select()
+    .from(usersTable)
+    .where(eq(usersTable.email, email))
+    .limit(1)
+
+  if (!user) {
+    return res.send("User not found");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password!)
+
+  if (!isMatch) {
+
+    return res.send("Emanil and password are incorrect")
+  }
+
+  // res.json({
+  //   success: true,
+  //   message: "Login successful"
+  // });
+
+  res.redirect("/consent.html");
+
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
