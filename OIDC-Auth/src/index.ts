8d6ac913e273c884dc0b44121b3d32aaf7e92b5c
Manpreet Singh <mrmanpreet073@gmail.com>
2026-05-21 00:38:12 +0530

    OIDC done

--- OIDC-Auth/src/index.ts
+++ OIDC-Auth/src/index.ts
@@ -4,9 +4,15 @@ import { usersTable } from "./db/schema.js";
 import { eq } from "drizzle-orm";
 import { db } from "./db/index.js";
 import bcrypt from "bcrypt"
+import { PRIVATE_KEY, PUBLIC_KEY } from "./utils/cert.js";
+import JWT from "jsonwebtoken";
+import type { JWTClaims } from "./utils/jwtTypes.js";
+import cookieParser from "cookie-parser";
+import jose from "node-jose";
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -25,7 +31,7 @@ app.get("/.well-known/openid-configuration", (req: Request, res: Response) => {
   const ISSUER = `http://localhost:${PORT}`;
   return res.json({
     issuer: ISSUER,
-    authorization_endpoint: `${ISSUER}/o/login`,
+    authorization_endpoint: `${ISSUER}/o/authorize`,
     userinfo_endpoint: `${ISSUER}/o/userinfo`,
     jwks_uri: `${ISSUER}/.well-known/jwks.json`,
   });
@@ -34,15 +40,10 @@ app.get("/.well-known/openid-configuration", (req: Request, res: Response) => {
 app.get("/home", (req: Request, res: Response) => {
   res.sendFile("home.html", { root: "./public" });
 });
+
 app.get("/signup", (req: Request, res: Response) => {
   res.sendFile("signup.html", { root: "./public" });
 });
-app.get("/login", (req: Request, res: Response) => {
-  res.sendFile("login.html", { root: "./public" });
-});
-
-
-
 
 app.post("/signup", async (req: Request, res: Response) => {
 
@@ -72,7 +73,11 @@ app.post("/signup", async (req: Request, res: Response) => {
 });
 
 
-app.post("/o/login", async (req: Request, res: Response) => {
+app.get("/o/authorize", (req: Request, res: Response) => {
+  res.sendFile("login.html", { root: "./public" });
+});
+
+app.post("/o/authorize", async (req: Request, res: Response) => {
 
   const { email, password } = req.body;
   if (!email || !password) {
@@ -95,14 +100,91 @@ app.post("/o/login", async (req: Request, res: Response) => {
     return res.send("Emanil and password are incorrect")
   }
 
-  // res.json({
-  //   success: true,
-  //   message: "Login successful"
-  // });
+  const ISSUER = `http://localhost:${PORT}`;
+  const now = Math.floor(Date.now() / 1000);
 
+  const claims: JWTClaims = {
+    iss: ISSUER,
+    sub: user.id,
+    email: user.email,
+    email_verified: String(user.emailVerified),
+    exp: now + 3600,
+    given_name: user.firstName ?? "",
+    family_name: user.lastName ?? undefined,
+    name: [user.firstName, user.lastName].filter(Boolean).join(" "),
+    picture: user.profileImageURL ?? undefined,
+  };
+  const token = JWT.sign(claims, PRIVATE_KEY, { algorithm: "RS256" });
+  //            -asymmetric cryptography: -->                ========  
+  //               Private Key ->	Sign
+  //               Public  Key ->	Verify
+
+
+  // Store token in cookie
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: false,
+    sameSite: "lax",
+    maxAge: 1000 * 60 * 60
+  });
+
+  // Redirect to consent page
   res.redirect("/consent.html");
 
+
+  // res.json({ token });
+  // return res.redirect("/consent");
+
 })
+
+
+app.get("/o/userinfo", (req: Request, res: Response) => {
+
+  // Read Authorization header
+  const authHeader = req.headers.authorization;
+
+  // Check if header exists
+  if (!authHeader) {
+    return res.status(401).send("Authorization header missing");
+  }
+
+  // Check Bearer format
+  if (!authHeader.startsWith("Bearer ")) {
+    return res.status(401).send("Invalid authorization format");
+  }
+
+  // Extract token
+  const token = authHeader.split(" ")[1];
+
+  if(!token){
+    return res.status(401).send("Token missing");
+  }
+  try {
+
+    // Verify JWT using PUBLIC KEY
+    const decoded = JWT.verify(token, PUBLIC_KEY, {
+      algorithms: ["RS256"]
+    });
+
+    // Return user info
+    return res.json(decoded);
+
+  } catch (error) {
+
+    return res.status(401).send("Invalid token");
+
+  }
+});
+
+app.get("/.well-known/jwks.json", async (_, res) => {
+  const key = await jose.JWK.asKey(PUBLIC_KEY, "pem");
+  //                         👆          👆      👆
+  //                    converts it   your key  tells jose
+  //                    to JWK format  (Buffer   the format
+  //
+  return res.json({ keys: [key.toJSON()] });
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
