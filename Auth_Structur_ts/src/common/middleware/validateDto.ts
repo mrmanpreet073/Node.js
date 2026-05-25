2b80f9f074a00dd5ab334bd3cd69f6f4a6b8d797
Manpreet Singh <mrmanpreet073@gmail.com>
2026-05-25 18:41:28 +0530

    middleware

---
+++ Auth_Structur_ts/src/common/middleware/validateDto.ts
@@ -0,0 +1,35 @@
+import type { Request, Response, NextFunction } from "express";
+import apiError from "../utils/apiError.js";
+
+type DTOValidator = {
+    validate: (data: unknown) => {
+        errors?: string[];
+        value: unknown;
+    };
+};
+
+function validate(dtoClass: DTOValidator) {
+
+    return ( req: Request, res: Response, next: NextFunction): void => {
+
+        try {
+
+            const { errors, value } = dtoClass.validate(req.body);
+
+            if (errors) {
+                return next(apiError.badRequest( "Validation Error: " + errors.join(", ") ));
+            }
+
+            req.body = value;
+
+            next();
+
+        } catch (err) {
+
+            next(err);
+
+        }
+    };
+}
+
+export default validate;
