541e154ec1c0bc6c9a574e3f79bb8497cdecc73b
Manpreet Singh <mrmanpreet073@gmail.com>
2026-05-28 01:32:02 +0530

    add VerificationToken Expiry and set isVerified Boolean

--- Auth_Structur_ts/src/db/authSchema.ts
+++ Auth_Structur_ts/src/db/authSchema.ts
@@ -1,4 +1,4 @@
-import { pgTable, serial, varchar, timestamp, pgEnum } from "drizzle-orm/pg-core";
+import { pgTable, serial, varchar, timestamp, pgEnum, boolean } from "drizzle-orm/pg-core";
 
 export const roleEnum = pgEnum("role", [
     "customer",
@@ -13,11 +13,12 @@ export const authUsers = pgTable("authUsers", {
     email: varchar("email", { length: 255 }).notNull().unique(),
     password: varchar("password", { length: 255 }).notNull(),
     role: roleEnum("role").default("customer"),
-    isVerified: varchar("isVerified", { length: 5 }).notNull().default("false"),
+    isVerified: boolean("isVerified").default(false).notNull(),
     verificationToken: varchar("verificationToken", { length: 255 }),
+    verificationTokenExpiresAt:timestamp("verification_token_expiry"),
     refreshToken: varchar("refreshToken", { length: 255 }),
     resetPasswordToken: varchar("resetPasswordToken", { length: 255 }),
-    reserPasswordExpiry: varchar("reserPasswordExpiry", { length: 255 }),
+    resetPasswordExpiry: timestamp("reset_password_expiry"),
     createdAt: timestamp("createdAt").defaultNow().notNull(),
     updatedAt: timestamp("updatedAt").defaultNow().notNull(),
 })
