40eb0eb2c4033e4f795ed995e7ddba757a61dda3
Manpreet Singh <mrmanpreet073@gmail.com>
2026-05-28 16:03:38 +0530

    cookie parser

--- Auth_Structur_ts/src/index.ts
+++ Auth_Structur_ts/src/index.ts
@@ -4,11 +4,13 @@ import "dotenv/config";
 import { db } from "./db/index.js";
 import { users } from "./db/schema.js";
 import authRouter from "./modules/auth/auth.routes.js";
+import cookieParser from "cookie-parser";
 
 const app: Application = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public")); // Serve static files from the "public" directory
+app.use(cookieParser());
 
 // Routers 
 app.use("/auth",authRouter);
